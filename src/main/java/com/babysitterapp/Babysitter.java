126151f193a6fbdf7ee9c3bf6746b25f422f065e
Nick Amick <nicholas.a.amick@gmail.com>
2022-01-21 17:15:01 -0500

    half night work test passing

--- src/main/java/com/babysitterapp/Babysitter.java
+++ src/main/java/com/babysitterapp/Babysitter.java
@@ -6,23 +6,31 @@ public class Babysitter {
 
     public int calculateRate(double startTime, double endTime) {
         int rate = 0;
-        // Add $12 to the rate for each hr worked before bedtime
-        while (startTime < 9) {
+        // Taking into account the 24 hr clock
+        if (endTime < 5) {
+            endTime += 12;
+        }
+        // Add $12 to the rate for each hr worked before bedtime and end time
+        while (futureStartTime(startTime) <= 9 && futureStartTime(startTime) <= endTime) {
             startTime++;
             rate += 12;
         }
-        // Add $8 to the rate for each hr worked before midnight
-        while (startTime < 12) {
+        // Add $8 to the rate for each hr worked before midnight and end time
+        while (futureStartTime(startTime) <= 12 && futureStartTime(startTime) <= endTime) {
             startTime++;
             rate += 8;
         }
-        // Set value of start time to 0 to count for after midnight hours
-        startTime = 0;
-        // Add $16 to the rate for each hr worked after midnight
-        while (startTime < 4) {
+        // Add $16 to the rate for each hr worked after midnight and end time
+        while (futureStartTime(startTime) <= 16 && futureStartTime(startTime) <= endTime) {
             startTime++;
             rate += 16;
         }
         return rate;
+    }
+
+    // Method that returns the start time with an added hour
+    // To compare if a whole hour has been worked
+    private double futureStartTime(double startTime) {
+        return startTime + 1;
     }
 }
