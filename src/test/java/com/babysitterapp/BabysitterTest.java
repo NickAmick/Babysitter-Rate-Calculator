d6dabdd18ed560891336a2fc113f4a29970fa165
Nick Amick <nicholas.a.amick@gmail.com>
2022-01-21 17:21:16 -0500

    6:15PM to 3AM test added and passed

--- src/test/java/com/babysitterapp/BabysitterTest.java
+++ src/test/java/com/babysitterapp/BabysitterTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 
 public class BabysitterTest {
 
-    private Babysitter babysitter = new Babysitter();
+    private final Babysitter babysitter = new Babysitter();
 
     @Test
     public void calculate_rate_for_full_days_work_5_to_4() {
@@ -22,13 +22,28 @@ public class BabysitterTest {
     }
 
     @Test
-    public void caluclate_rate_for_half_night_work_5_to_9() {
+    public void calculate_rate_for_half_night_work_5_to_9() {
         // 5PM - 9PM is 4 hrs at $12/hr equals $48
 
         // Arrange
         int expectedRate = 48;
         // Act
         int actualRate = babysitter.calculateRate(5, 9);
+        // Assert
+        Assert.assertEquals(expectedRate, actualRate);
+    }
+
+    @Test
+    public void calculate_rate_for_615_to_3() {
+        // 6:15PM - 9PM is 2 full hrs at $12/hr equals $24
+        // 9PM - Midnight is 3 hrs at $8/hr equals $24
+        // Midnight - 3AM is 3 hrs at $16/hr equals $48
+        // Total is $96
+
+        // Arrange
+        int expectedRate = 96;
+        // Act
+        int actualRate = babysitter.calculateRate(6.25, 3);
         // Assert
         Assert.assertEquals(expectedRate, actualRate);
     }
