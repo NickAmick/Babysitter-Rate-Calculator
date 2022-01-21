ffc510df7664b4da49d9b5effea32d264a0c11d3
Nick Amick <nicholas.a.amick@gmail.com>
2022-01-21 18:45:35 -0500

    later than midnight bedtime test, failing

--- src/test/java/com/babysitterapp/BabysitterTest.java
+++ src/test/java/com/babysitterapp/BabysitterTest.java
@@ -8,7 +8,7 @@ public class BabysitterTest {
 
     @Test
     public void calculate_rate_for_full_days_work_5_to_4() {
-        // 5PM - 9PM is 4 hrs at $12/hr equals $48
+        // 5PM - bedtime 9PM is 4 hrs at $12/hr equals $48
         // 9PM - Midnight is 3 hrs at $8/hr equals $24
         // Midnight - 4AM is 4 hrs at $16/hr equals $64
         // Total is $136
@@ -24,7 +24,7 @@ public class BabysitterTest {
 
     @Test
     public void calculate_rate_for_half_night_work_5_to_9() {
-        // 5PM - 9PM is 4 hrs at $12/hr equals $48
+        // 5PM - bedtime 9PM is 4 hrs at $12/hr equals $48
 
         // Arrange
         int expectedRate = 48;
@@ -37,7 +37,7 @@ public class BabysitterTest {
 
     @Test
     public void calculate_rate_for_615_to_3() {
-        // 6:15PM - 9PM is 2 full hrs at $12/hr equals $24
+        // 6:15PM - bedtime 9PM is 2 full hrs at $12/hr equals $24
         // 9PM - Midnight is 3 hrs at $8/hr equals $24
         // Midnight - 3AM is 3 hrs at $16/hr equals $48
         // Total is $96
@@ -47,6 +47,21 @@ public class BabysitterTest {
         // Act
         babysitter = new Babysitter(9);
         int actualRate = babysitter.calculateRate(6.25, 3);
+        // Assert
+        Assert.assertEquals(expectedRate, actualRate);
+    }
+
+    @Test
+    public void calculate_rate_bedtime_1_shift_7_to_2() {
+        // 7PM - bedtime 1AM is 5 hrs at $12/hr rate equals $60
+        // Midnight - 2AM is 2 hrs at $16/hr equals $32
+        // Total is $92
+
+        // Arrange
+        int expectedRate = 92;
+        // Act
+        babysitter = new Babysitter(1);
+        int actualRate = babysitter.calculateRate(7, 2);
         // Assert
         Assert.assertEquals(expectedRate, actualRate);
     }
