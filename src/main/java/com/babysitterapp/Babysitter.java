fe3e21a400125f146043baeee5f9e3a7ac4f451a
Nick Amick <nicholas.a.amick@gmail.com>
2022-01-21 18:58:05 -0500

    late bedtime test passing, added getter and setter

--- src/main/java/com/babysitterapp/Babysitter.java
+++ src/main/java/com/babysitterapp/Babysitter.java
@@ -3,7 +3,7 @@ package com.babysitterapp;
 public class Babysitter {
 
     // Bedtime may vary, needed for user input
-    private final double bedtime;
+    private double bedtime;
     public Babysitter(double bedtime) {
         this.bedtime = bedtime;
     }
@@ -14,10 +14,16 @@ public class Babysitter {
         if (endTime < 5) {
             endTime += 12;
         }
+        if (getBedtime() < 5) {
+            setBedtime(this.bedtime += 12);
+        }
         // Add $12 to the rate for each hr worked before bedtime and end time
-        while (futureStartTime(startTime) <= this.bedtime && futureStartTime(startTime) <= endTime) {
+        while (futureStartTime(startTime) <= getBedtime() && futureStartTime(startTime) <= endTime) {
             startTime++;
             rate += 12;
+            if (futureStartTime(startTime) > 12) {
+                break;
+            }
         }
         // Add $8 to the rate for each hr worked before midnight and end time
         while (futureStartTime(startTime) <= 12 && futureStartTime(startTime) <= endTime) {
@@ -36,5 +42,13 @@ public class Babysitter {
     // To compare if a whole hour has been worked
     private double futureStartTime(double startTime) {
         return startTime + 1;
+    }
+
+    public double getBedtime() {
+        return bedtime;
+    }
+
+    public void setBedtime(double bedtime) {
+        this.bedtime = bedtime;
     }
 }
